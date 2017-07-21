a470d3310321ad848066424cd275242c7dd3eae3
Godning <736397792@qq.com>
2017-07-21 17:14:07 +0000

    update section-3

--- practice/section-3/practice-1.js
+++ practice/section-3/practice-1.js
@@ -2,4 +2,10 @@
 
 function createUpdatedCollection(collectionA, objectB) {
   //return '实现练习要求，并改写该行代码。';
+  [...collectionA].forEach(function(x){
+    if(objectB.value.find((n) => n == x.key)){
+      x.count--;
+    }
+  });
+  return collectionA;
 }
